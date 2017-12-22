1b3787354881e535fd2dc611dfe983438abdf497
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-12-22 17:45:59 +0000

    Update to Step methods

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,2 +1,36 @@
 class StepsController < ApplicationController
+  def edit
+    @tasklist = Tasklist.find(params[:tasklist_id])
+    @step = @tasklist.steps.find(params[:id])
+  end
+
+  def create
+    @tasklist = Tasklist.find(params[:tasklist_id])
+    @step = @tasklist.steps.create(step_params)
+    redirect_to tasklist_path(@tasklist)
+  end
+
+  def update
+    @tasklist = Tasklist.find(params[:tasklist_id])
+     @step = @tasklist.steps.find(params[:id])
+
+      if @step.update(step_params)
+        redirect_to tasklists_path
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @tasklist = Tasklist.find(params[:tasklist_id])
+    @step = @tasklist.steps.find(params[:id])
+    @step.destroy
+
+    redirect_to tasklist_path(@tasklist)
+  end
+
+  private
+  def step_params
+    params.require(:step).permit(:title, :step_number, :description, :optional)
+  end
 end
