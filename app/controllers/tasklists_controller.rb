d7a7f742592630a324ad01f26207c1ac6555f874
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-12-22 16:22:19 +0000

    updates to tasklist methods

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -10,11 +10,33 @@ class TasklistsController < ApplicationController
   def new
   end
 
+  def edit
+    @tasklist = Tasklist.find(params[:id])
+  end
+
   def create
     # render plain: params[:tasklist].inspect
-    @tasklist = Tasklist.new(params.require(:tasklist).permit(:title, :description, :base_path, :meta_description))
+    @tasklist = Tasklist.new(tasklist_params)
 
-    @tasklist.save
-    redirect_to @tasklist
+    if @tasklist.save
+      redirect_to @tasklist
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @tasklist = Tasklist.find(params[:id])
+
+    if @tasklist.update(tasklist_params)
+      redirect_to @tasklist
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def tasklist_params
+    params.require(:tasklist).permit(:title, :description, :base_path, :meta_description)
   end
 end
--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,2 +1,5 @@
 class Tasklist < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :base_path, presence: true
+  validates :description, presence: true, length: {minimum: 10}
 end
