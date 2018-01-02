39e2c8af1fa9ffcc971b91d62c1a9a3233575143
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2018-01-02 19:13:05 +0000

    Add position field to Content model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171222215404) do
+ActiveRecord::Schema.define(version: 20180102190123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171222215404) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "content_type"
+    t.integer  "position"
     t.index ["step_id"], name: "index_contents_on_step_id", using: :btree
   end
 
