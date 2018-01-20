161b6be3b2b02118c97709d0367b65a7a3eaea4f
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2018-01-20 00:52:02 +0000

    Tweak to json toggle button

--- app/assets/javascripts/tasklists.js
+++ app/assets/javascripts/tasklists.js
@@ -3,8 +3,10 @@ function toggleJSON() {
     var json_content = document.getElementById("json_content");
     if (json_content.style.display === "none") {
         json_content.style.display = "block";
+        $("#json_button").text("Hide json");
     } else {
         json_content.style.display = "none";
+        $("#json_button").text("Show json");
     }
 }
 
